b3adf562269aa9d3cad92add1f145d51071afdf3
bhatiya123 <bhatiyahet882@gmail.com>
2024-12-24 11:56:24 +0530

    admin layout

---
+++ admin/src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/AppSidebar";
+
+export default function Layout({ children }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+}
