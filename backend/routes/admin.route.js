2fef043c5ac61ef1668b715bb0d2854fdb2160c7
Kenil Gondaliya <kenilgondaliya960gmail.com>
2024-12-26 22:23:43 +0530

    commit

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,25 +1,27 @@
-import express from 'express';
-import { LoginAdmin,AddProudct,getAllProducts,OatDrink } from '../controllers/admin.controllers.js';
-import { upload } from "../middleware/multer.js"
-
+import express from "express";
+import {
+  LoginAdmin,
+  AddProudct,
+  getAllProducts,
+  OatDrink,
+} from "../controllers/admin.controllers.js";
+import { upload } from "../middleware/multer.js";
 
 const router = express.Router();
 
 router.post("/login", LoginAdmin);
 
 router.route("/addProduct").post(
-    upload.fields([
-        {
-            name: "image",
-            maxcount:1,
-        }
-    ]), 
-    AddProudct
-)
+  upload.fields([
+    {
+      name: "image",
+      maxcount: 1,
+    },
+  ]),
+  AddProudct
+);
 
 router.get("/getAllProduct", getAllProducts);
-router.get("/oatdrink", OatDrink);
-
-
+router.get("/oatDrink", OatDrink);
 
 export default router;
