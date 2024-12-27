78876ce271bdd8e40a9d650a8e4939a9d119ac92
raviitaliya <hello@raviitaliya.info>
2024-12-27 23:25:56 +0530

    [fix]-try to fix bug but i faild

--- frontend/src/pages/ViewProduct.jsx
+++ frontend/src/pages/ViewProduct.jsx
@@ -8,15 +8,14 @@ function ViewProduct() {
 
   const { id } = useParams('id');
 
-  
-
 
   const { oneProduct, loading, error, getOneProduct } = useProductStore();
 
   useEffect(() => {
+    // Clear the previous product's data before fetching a new one
+    setoneProduct(null);
     getOneProduct(id);
-  }, [id, getOneProduct]);
-
+  }, [id, getOneProduct, setoneProduct]);
 
 
 
