b7f12b18fc64deabdafe64a8b72e2f3bbef06e9a
Kenil Gondaliya <kenilgondaliya960gmail.com>
2024-12-30 12:28:50 +0530

    commit

--- frontend/src/pages/ViewProduct.jsx
+++ frontend/src/pages/ViewProduct.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
-import { useParams, Link ,useNavigate} from "react-router-dom";
+import  { useEffect } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import { useProductStore } from "../store/Store";
+import nice from "../assets/ProductSvg/nice.svg";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -9,25 +10,21 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 
-
 function ViewProduct() {
   const navigate = useNavigate();
   const { id, category } = useParams();
-  const { oneProduct, loading, error, getOneProduct, setClearState } = useProductStore();
-
-  
+  const { oneProduct, loading, error, getOneProduct, setClearState } =
+    useProductStore();
 
   const handleNavigate = (path) => {
     navigate(path);
   };
   useEffect(() => {
-
     if (oneProduct && oneProduct._id === id) return;
 
     setClearState();
     getOneProduct(id);
   }, [id, oneProduct, getOneProduct, setClearState]);
-
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
@@ -35,37 +32,63 @@ function ViewProduct() {
 
   return (
     <div className="">
+      <Breadcrumb className="mb-6 z-50 relative !mt-[-66px] ml-16 font-font2">
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink as={Link} to="/">
+              Home
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink onClick={() => handleNavigate("/our-products")}>
+              Products
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink
+              onClick={() =>
+                handleNavigate(
+                  `/our-products/${oneProduct.category
+                    .replace(/\s+/g, "-")
+                    .toLowerCase()}`
+                )
+              }
+            >
+              {oneProduct.category}
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink>{oneProduct.productname}</BreadcrumbLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
 
-<Breadcrumb className="mb-6 z-50 relative !mt-[-66px] ml-16 font-font2">
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink as={Link} to="/">
-                Home
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbLink onClick={() => handleNavigate('/our-products')} >
-                Products
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbLink onClick={() => handleNavigate(`/our-products/${oneProduct.category.replace(/\s+/g, '-').toLowerCase()}`)}>
-                {oneProduct.category}
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbLink>{oneProduct.productname}</BreadcrumbLink>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
-   
-
-      <img src={oneProduct.image} alt={oneProduct.productname || "Product"} />
+      {/* 
       <h1>{oneProduct.productname}</h1>
-      <p>{oneProduct.description}</p>
+      <p>{oneProduct.description}</p> */}
+      {/*  */}
+
+      <div className="flex items-center justify-center">
+        <div className="flex flex-col">
+          <p>{oneProduct.productname}</p>
+          <p>{oneProduct.desription}</p>
+        </div>
+        <div className="flex">
+          <div className="relative">
+            <img
+              src={oneProduct.image}
+              alt={oneProduct.productname || "Product"}
+            />
+          </div>
+
+          {/* <div className="absolute top-0 ">
+            <img src={nice} className="w-12 " alt="this is nice svg" />
+          </div> */}
+        </div>
+      </div>
     </div>
   );
 }
