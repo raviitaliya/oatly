93d7d5cb905c175a0b8a6926a6bb76e07fb598fb
Kenil Gondaliya <kenilgondaliya960gmail.com>
2025-01-12 21:28:53 +0530

    commit

--- frontend/src/components/ui/AddToCardBtn.jsx
+++ frontend/src/components/ui/AddToCardBtn.jsx
@@ -1,7 +1,10 @@
-function AddToCardBtn({onClick}) {
+function AddToCardBtn({ onClick }) {
   return (
     <div>
-      <button onClick={onClick} className="bg-black hover:bg-black  text-white font-font1 text-pretty w-64 py-2 px-4 rounded">
+      <button
+        onClick={onClick}
+        className="bg-black hover:bg-black  text-white font-font1 text-pretty w-64 py-2 px-4 rounded"
+      >
         Add To Card
       </button>
     </div>
