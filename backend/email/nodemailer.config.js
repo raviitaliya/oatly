9da773d43ec3effa7978a47abb8805447c1e4cea
Kenil Gondaliya <kenilgondaliya960gmail.com>
2025-01-25 13:15:48 +0530

    commit

--- backend/email/nodemailer.config.js
+++ backend/email/nodemailer.config.js
@@ -4,17 +4,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com", // You can use any SMTP service like Mailgun, SendGrid, etc.
-    port: 587, // Or 465 for secure SSL connection
-    secure: false, // Use SSL if port 465 is used
+    host: "smtp.gmail.com", 
+    port: 587, 
+    secure: false, 
     auth: {
-      user: process.env.EMAIL_USER,  // Your email address
-      pass: process.env.EMAIL_PASS,  // Your email password
+      user: process.env.EMAIL_USER,  
+      pass: process.env.EMAIL_PASS,  
     },
   });
   
   export const sender = {
-    email: process.env.EMAIL_USER,  // Email from which emails will be sent
+    email: process.env.EMAIL_USER,  
     name: "Oatly",
   };
   
