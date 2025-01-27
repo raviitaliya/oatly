f2482ab34825aa666314493be7835a74819a36ab
raviitaliya <hello@raviitaliya.info>
2025-01-27 23:14:31 +0530

    [fix]-sign up page

--- frontend/src/auth/SignUp.jsx
+++ frontend/src/auth/SignUp.jsx
@@ -34,8 +34,8 @@ const SignUp = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-   const response = await signUpUser(formData);
-  //  console.log("sucessssssss")
+    const response = await signUpUser(formData);
+    //  console.log("sucessssssss")
     if (response && !error) {
       navigate("/otp");
     } else {
@@ -57,8 +57,8 @@ const SignUp = () => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4">
-      <div className="bg-white w-full max-w-3xl relative border-2 border-dashed  border-gray-400">
-        <div className="p-8">
+      <div className="bg-white w-full max-w-3xl relative border-2 border-dashed border-gray-400">
+        <div className="p-8  ">
           <h2 className="text-4xl font-bold text-center font-font1 tracking-wide mb-6">
             Become a Member
           </h2>
@@ -67,9 +67,11 @@ const SignUp = () => {
             eco-friendly dairy alternatives for all.
           </h2>
 
-          <form className="space-y-6" onSubmit={handleSubmit}>
+          <form className="space-y-6 " onSubmit={handleSubmit}>
             {step === 1 && (
-              <>
+              <div className="h-[450px] flex flex-col justify-between">
+                <div className="space-y-6">
+
                 <div>
                   <label className="block text-xl font-font2 mb-1">Name</label>
                   <input
@@ -124,7 +126,10 @@ const SignUp = () => {
                   />
                 </div>
 
-                <div className="flex justify-between">
+                  
+                </div>
+
+                <div className="flex justify-between items-end h-full">
                   <button
                     type="button"
                     className="w-24 bg-black text-white py-3 rounded font-bold hover:bg-gray-800 transition-colors"
@@ -140,173 +145,183 @@ const SignUp = () => {
                     Next
                   </button>
                 </div>
-              </>
+              </div>
             )}
 
             {step === 2 && (
-              <>
-                <div>
-                  <label className="block text-xl font-font2 mb-1">
-                    Primary Mobile
-                  </label>
-                  <input
-                    type="text"
-                    name="primaryMobile"
-                    value={formData.primaryMobile}
-                    onChange={handleChange}
-                    className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent font-font2 text-xl"
-                    placeholder="Enter your primary mobile number"
-                  />
+              <div className=" h-[450px] flex flex-col justify-between  ">
+
+                <div className="space-y-6">
+                  <div>
+                    <label className="block text-xl font-font2 mb-1">
+                      Primary Mobile
+                    </label>
+                    <input
+                      type="text"
+                      name="primaryMobile"
+                      value={formData.primaryMobile}
+                      onChange={handleChange}
+                      className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent font-font2 text-xl"
+                      placeholder="Enter your primary mobile number"
+                    />
+                  </div>
+
+                  <div>
+                    <label className="block text-xl font-font2 mb-1">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      name="password"
+                      value={formData.password}
+                      onChange={handleChange}
+                      className="w-full px-3 py-[6px] border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent font-font2 text-xl"
+                      placeholder="Enter your password"
+                    />
+                  </div>
+
+                  <div>
+                    <label className="block text-xl font-font2 mb-1">
+                      Confirm Password
+                    </label>
+                    <input
+                      type="password"
+                      name="confpassword"
+                      value={formData.confpassword}
+                      onChange={handleChange}
+                      className="w-full px-3 py-[6px] border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent font-font2 text-xl"
+                      placeholder="Confirm your password"
+                    />
+                  </div>
                 </div>
 
-                <div>
-                  <label className="block text-xl font-font2 mb-1">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    value={formData.password}
-                    onChange={handleChange}
-                    className="w-full px-3 py-[6px] border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent font-font2 text-xl"
-                    placeholder="Enter your password"
-                  />
+
+                <div className="">
+                  <div className="flex justify-between">
+                    <button
+                      type="button"
+                      className="w-24 bg-black text-white py-3 rounded font-bold hover:bg-gray-400 transition-colors"
+                      onClick={handlePrevious}
+                    >
+                      Back
+                    </button>
+                    <button
+                      type="button"
+                      className="w-24 bg-black text-white py-3 rounded font-bold hover:bg-gray-800 transition-colors"
+                      onClick={handleNext}
+                    >
+                      Next
+                    </button>
+                  </div>
                 </div>
 
-                <div>
-                  <label className="block text-xl font-font2 mb-1">
-                    Confirm Password
-                  </label>
-                  <input
-                    type="password"
-                    name="confpassword"
-                    value={formData.confpassword}
-                    onChange={handleChange}
-                    className="w-full px-3 py-[6px] border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent font-font2 text-xl"
-                    placeholder="Confirm your password"
-                  />
-                </div>
-
-                <div className="flex justify-between">
-                  <button
-                    type="button"
-                    className="w-24 bg-black text-white py-3 rounded font-bold hover:bg-gray-400 transition-colors"
-                    onClick={handlePrevious}
-                  >
-                    Back
-                  </button>
-                  <button
-                    type="button"
-                    className="w-24 bg-black text-white py-3 rounded font-bold hover:bg-gray-800 transition-colors"
-                    onClick={handleNext}
-                  >
-                    Next
-                  </button>
-                </div>
-              </>
+              </div>
             )}
 
             {step === 3 && (
               <>
-                <div>
-                  <label className="block text-xl font-font2 mb-1">
-                    Address Line 1
-                  </label>
-                  <input
-                    type="text"
-                    name="address1"
-                    value={formData.address1}
-                    onChange={handleChange}
-                    className="w-full px-3 py-[6px] border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent font-font2 text-xl"
-                    placeholder="Enter your address"
-                  />
-                </div>
+                <div className="">
+                  <div className="space-y-6 overflow-scroll h-[400px]">
 
-                <div>
-                  <label className="block text-xl font-font2 mb-1">
-                    Address Line 2
-                  </label>
-                  <input
-                    type="text"
-                    name="address2"
-                    value={formData.address2}
-                    onChange={handleChange}
-                    className="w-full px-3 py-[6px] border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent font-font2 text-xl"
-                    placeholder="Enter your secondary address"
-                  />
-                </div>
+                    <div>
+                      <label className="block text-xl font-font2 mb-1">
+                        Address Line 1
+                      </label>
+                      <input
+                        type="text"
+                        name="address1"
+                        value={formData.address1}
+                        onChange={handleChange}
+                        className="w-full px-3 py-[6px] border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent font-font2 text-xl"
+                        placeholder="Enter your address"
+                      />
+                    </div>
 
-                <div>
-                  <label className="block text-xl font-font2 mb-1">City</label>
-                  <input
-                    type="text"
-                    name="city"
-                    value={formData.city}
-                    onChange={handleChange}
-                    className="w-full px-3 py-[6px] border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent font-font2 text-xl"
-                    placeholder="Enter your city"
-                  />
-                </div>
+                    <div>
+                      <label className="block text-xl font-font2 mb-1">
+                        Address Line 2
+                      </label>
+                      <input
+                        type="text"
+                        name="address2"
+                        value={formData.address2}
+                        onChange={handleChange}
+                        className="w-full px-3 py-[6px] border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent font-font2 text-xl"
+                        placeholder="Enter your secondary address"
+                      />
+                    </div>
 
-                <div>
-                  <label className="block text-xl font-font2 mb-1">
-                    State
-                  </label>
-                  <input
-                    type="text"
-                    name="state"
-                    value={formData.state}
-                    onChange={handleChange}
-                    className="w-full px-3 py-[6px] border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent font-font2 text-xl"
-                    placeholder="Enter your state"
-                  />
-                </div>
+                    <div>
+                      <label className="block text-xl font-font2 mb-1">City</label>
+                      <input
+                        type="text"
+                        name="city"
+                        value={formData.city}
+                        onChange={handleChange}
+                        className="w-full px-3 py-[6px] border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent font-font2 text-xl"
+                        placeholder="Enter your city"
+                      />
+                    </div>
 
-                <div>
-                  <label className="block text-xl font-font2 mb-1">
-                    Country
-                  </label>
-                  <input
-                    type="text"
-                    name="country"
-                    value={formData.country}
-                    onChange={handleChange}
-                    className="w-full px-3 py-[6px] border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent font-font2 text-xl"
-                    placeholder="Enter your country"
-                  />
-                </div>
+                    <div>
+                      <label className="block text-xl font-font2 mb-1">
+                        State
+                      </label>
+                      <input
+                        type="text"
+                        name="state"
+                        value={formData.state}
+                        onChange={handleChange}
+                        className="w-full px-3 py-[6px] border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent font-font2 text-xl"
+                        placeholder="Enter your state"
+                      />
+                    </div>
 
-                <div>
-                  <label className="block text-xl font-font2 mb-1">
-                    Zipcode
-                  </label>
-                  <input
-                    type="text"
-                    name="zipcode"
-                    value={formData.zipcode}
-                    onChange={handleChange}
-                    className="w-full px-3 py-[6px] border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent font-font2 text-xl"
-                    placeholder="Enter your zipcode"
-                  />
-                </div>
+                    <div>
+                      <label className="block text-xl font-font2 mb-1">
+                        Country
+                      </label>
+                      <input
+                        type="text"
+                        name="country"
+                        value={formData.country}
+                        onChange={handleChange}
+                        className="w-full px-3 py-[6px] border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent font-font2 text-xl"
+                        placeholder="Enter your country"
+                      />
+                    </div>
 
-                <div className="flex justify-between">
-                  <button
-                    type="button"
-                    className="w-24 bg-black text-white py-3 rounded font-bold hover:bg-gray-400 transition-colors"
-                    onClick={handlePrevious}
-                  >
-                    Back
-                  </button>
-                  
-                  
-                  <button
-                    type="submit"
-                    className="w-24 bg-black text-white py-3 rounded font-bold hover:bg-gray-800 transition-colors"
-                  >
-                    Submit
-                  </button>
-              
+                    <div>
+                      <label className="block text-xl font-font2 mb-1">
+                        Zipcode
+                      </label>
+                      <input
+                        type="text"
+                        name="zipcode"
+                        value={formData.zipcode}
+                        onChange={handleChange}
+                        className="w-full px-3 py-[6px] border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent font-font2 text-xl"
+                        placeholder="Enter your zipcode"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex items-end justify-between">
+                    <button
+                      type="button"
+                      className="w-24 bg-black text-white py-3 rounded font-bold hover:bg-gray-400 transition-colors"
+                      onClick={handlePrevious}
+                    >
+                      Back
+                    </button>
+                    <button
+                      type="submit"
+                      className="w-24 bg-black text-white py-3 rounded font-bold hover:bg-gray-800 transition-colors"
+                    >
+                      Submit
+                    </button>
+                  </div>
+
                 </div>
               </>
             )}
