981925ddd364e7eaf70ed80244269ed91af9f2b0
Kenil Gondaliya <kenilgondaliya960gmail.com>
2025-03-09 17:10:31 +0530

    commit

---
+++ frontend/src/components/CanvasSection.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+
+const CanvasSection = () => {
+  return (
+    <div
+      className="relative bg-black "
+      style={{
+        backgroundImage: `url(https://a.storyblok.com/f/107921/1738x1200/df94971a12/black-bg2.gif)`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="relative mx-auto" style={{ width: "83.1168vh" }}>
+        <a
+          href="/oatly-who"
+          aria-label="The Oatly Sustainability report update 2022"
+          className="absolute block transition-transform hover:scale-105"
+          style={{ width: "59.94vh", left: "11.544vh", top: "5%" }}
+        >
+          <img
+            src="https://a.storyblok.com/f/107921/102x150/81f42ef337/logo-heading.svg"
+            alt="Oatly Sustainability Update 2023 - OMG! It's actually better this time."
+            className="w-full h-auto md:mt-7 lg:mt-14 xl:mt-8"
+            sizes="59.94vh"
+          />
+        </a>
+
+        {/* Hidden Pixel Image */}
+        <div
+          className="absolute"
+          style={{ width: "-1.2432vh", left: "84.36vh", top: "0%" }}
+        >
+          <img
+            src="https://a.storyblok.com/f/107921/1x1/ba8062f3dc/hidden-pixel.jpg/m/3840x0/filters:quality(75):format(webp)"
+            alt=""
+            role="presentation"
+            className="w-full h-auto"
+            sizes="-1.2432vh"
+          />
+        </div>
+
+        {/* Right Edge GIF */}
+        <div
+          className="absolute"
+          style={{ width: "3.2856vh", right: "-1.776vh", top: "-3%" }}
+        >
+          <img
+            src="https://a.storyblok.com/f/107921/51x1601/654222988d/edge.gif/m/48x0/filters:quality(75):format(webp)"
+            alt=""
+            role="presentation"
+            className="w-full h-auto"
+            sizes="3.2856vh"
+          />
+        </div>
+
+        {/* Left Edge GIF */}
+        <div
+          className="absolute"
+          style={{ width: "3.2856vh", left: "-1.776vh", top: "-3%" }}
+        >
+          <img
+            src="https://a.storyblok.com/f/107921/51x1601/bd217ddba2/edge-white-2.gif/m/48x0/filters:quality(75):format(webp)"
+            alt="this is images"
+            role="presentation"
+            className="w-full h-auto"
+            sizes="3.2856vh"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CanvasSection;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,11 @@
-
+// src/pages/Home.jsx
 import Navbar from "../components/Navbar";
 import { useProductStore } from "@/store/Store";
 import SignUp from "../auth/SignUp";
 import ResetPass from "../auth/ResetPass";
-import OtpPage from "./OtpPage"; 
+import OtpPage from "./OtpPage";
 import NewPassword from "./NewPassword";
+import CanvasSection from "@/components/CanvasSection";
 
 const Home = () => {
   const {
@@ -41,50 +42,7 @@ const Home = () => {
       onWheel={handleWheel}
     >
       <div className="w-full h-screen flex">
-        <div className="relative flex">
-          <img
-            src="https://a.storyblok.com/f/107921/51x1601/bd217ddba2/edge-white-2.gif/m/32x0/filters:quality(75):format(webp)"
-            alt="Oatly"
-            className="h-screen"
-          />
-          <img
-            src="https://a.storyblok.com/f/107921/1738x1200/df94971a12/black-bg2.gif"
-            className="w-[777px] h-[911px] object-cover"
-            alt="background"
-          />
-          <img
-            src="https://a.storyblok.com/f/107921/102x150/81f42ef337/logo-heading.svg"
-            className="absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 hover:scale-105 cursor-pointer transition-transform h-[803px]"
-            alt="logo"
-          />
-          <img
-            src="https://a.storyblok.com/f/107921/51x1601/654222988d/edge.gif/m/32x0/filters:quality(75):format(webp)"
-            className="absolute right-0 h-screen"
-            alt="edge-animation"
-          />
-        </div>
-
-        <div>
-          <div className="px-10">
-            <img
-              src="https://a.storyblok.com/f/107921/x/97e70255b8/feet.svg"
-              alt="Oatly"
-              className="h-[433px] w-[788px]"
-            />
-            <div className="flex gap-16">
-              <img
-                src="https://a.storyblok.com/f/107921/985x1564/90247dd5c8/barbershop-quartet-9.jpg/m/384x0/filters:quality(75):format(webp)"
-                className="w-[303px] h-[481px] object-cover"
-                alt="background"
-              />
-              <img
-                src="https://a.storyblok.com/f/107921/x/bd6be96854/milkmyths.svg"
-                className="w-[303px] h-[220px] object-cover mt-3 hover:scale-105 transition-transform"
-                alt="background"
-              />
-            </div>
-          </div>
-        </div>
+        <CanvasSection />
       </div>
 
       <div className="flex min-w-max gap-8 p-8">
